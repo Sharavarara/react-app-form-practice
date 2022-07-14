706ccf003f5eef7c1b1b8f3c8a6a5154121bd701
Rostyslav <perklmos@gmail.com>
2022-07-15 02:20:01 +0300

    Do refactoring styles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import styles from './../Home/Home.module.scss';
 
 const Home = () => {
   return (
-    <div className={styles.home}>
-      <p>Home</p>
-    </div>
+    <h1>HOME</h1>
   );
 }
 

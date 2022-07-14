568ddab2ac757e15c26612e6930ef9e31a18353d
Rostyslav <perklmos@gmail.com>
2022-07-15 02:16:03 +0300

    Add error messages for validation

--- src/utils/schemaValidation.js
+++ src/utils/schemaValidation.js
@@ -1,10 +1,10 @@
 import * as Yup from 'yup';
 
-const SCHEMA_NAME = Yup.string().matches(/^[A-Z][a-z]{1,20}$/)
+const SCHEMA_NAME = Yup.string().matches(/^[A-Z][a-z]{1,20}$/,'Field must match from 1 to 20 letters ').required("Field can't be empty");
 
-const SCHEMA_EMAIL = Yup.string().email('Email must be example@example.com').matches(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/).required();
+const SCHEMA_EMAIL = Yup.string().email('Email must be example@example.com').matches(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/).required("Field can't be empty");
 
-const SCHEMA_PASSWORD = Yup.string().required("Field can't be empty");
+const SCHEMA_PASSWORD = Yup.string().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,'Must match min 8 symbols,letters and nubmers').required("Field can't be empty");
 
 export const SCHEMA_LOG_IN = Yup.object({
   email:SCHEMA_EMAIL,
@@ -17,5 +17,5 @@ export const SCHEMA_SIGN_UP = Yup.object({
   dname:SCHEMA_NAME,
   email:SCHEMA_EMAIL,
   password:SCHEMA_PASSWORD,
-  passwordConf:SCHEMA_PASSWORD,
+  passwordConf:Yup.string().oneOf([Yup.ref('password')],'Passwords must match'),
 })
